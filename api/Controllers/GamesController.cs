e94ccff78bcf2df29ab008b7ec3a9ed6845fb250
Claudio <szclsb@gmail.com>
2021-12-03 14:26:52 +0100

    Prepared auth service

--- api/Controllers/GamesController.cs
+++ api/Controllers/GamesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace server.Controllers
+namespace ap.Controllers
 {
     [ApiController]
     [Route("api/games")]
--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MongoDB.Bson;
 using server.Models;
 using server.Services;
 
